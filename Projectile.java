0480434697e4230a86dd11f47da4825208ffe620
ShakyaMajumdar <69471777+ShakyaMajumdar@users.noreply.github.com>
2020-10-30 17:27:29 +0530

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -1,20 +1,23 @@
 import java.awt.*;
-class Projectile extends Gameplay
+class Projectile
 {
+    static int PANEL_WIDTH;
+    static int PANEL_HEIGHT;
+    static Image projectileImage;
+    static Gameplay imageObserver;
+    static int diameter = 20;    
     
     double x;
     double y;
     double velocityX;
     double velocityY;
-    int diameter;    
     
     public Projectile(int startX, int startY, int targetX, int targetY)
     {      
-        diameter = 20;
         x = startX;
         y = startY;
         double theta = Math.atan2(targetY-startY, targetX-startX);        
-        super.projectileImage = super.projectileImage.getScaledInstance(diameter,diameter,Image.SCALE_FAST);
+        //projectileImage = projectileImage.getScaledInstance(diameter+5,diameter+5,Image.SCALE_FAST);
         velocityX = (4*Math.cos(theta));
         velocityY = (4*Math.sin(theta));
     }
@@ -27,6 +30,6 @@ class Projectile extends Gameplay
     
     public void draw(Graphics g)
     {     
-        g.drawImage(super.projectileImage, (int)Math.floor(x)-diameter/2, (int)Math.floor(y)-diameter/2, this);
+        g.drawImage(projectileImage, (int)Math.floor(x)-diameter/2, (int)Math.floor(y)-diameter/2, imageObserver);
     }
 }
